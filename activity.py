26c0d1410ad5fd33a5ab157c9d946a3dc883318f
Sai Adwitiya Chiluveru <kcsabitha_mct@mgit.ac.in>
2026-03-18 18:46:07 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,8 @@
+Age = int(input("Enter your age: "))
+
+if Age < 10:
+    print("You are not allowed (Too Young!)")
+elif Age <= 20:
+    print("You are allowed in the class")
+else:
+    print("You are not allowed (Too Old!)")
