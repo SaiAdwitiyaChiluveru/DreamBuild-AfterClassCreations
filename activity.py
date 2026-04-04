72790180febe1b2e7868b74739e6e832984a9450
Sai Adwitiya Chiluveru <kcsabitha_mct@mgit.ac.in>
2026-04-04 13:18:08 +0530

    Add files via upload

--- activity.py
+++ activity.py
@@ -1,3 +1,3 @@
-number = float(input("Enter a number to find its square root: "))
-square_root = number ** 0.5
-print("The square root of", number, "is", square_root)
+word = input("Enter the word: ")
+upper_case_word = word.upper()
+print("Converted word:", upper_case_word)
